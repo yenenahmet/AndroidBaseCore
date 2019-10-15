2fe76e0cb9a5612171bb88b4b5bc2151d49833b8
Ahmet YENEN <ahmet.yenen@spexco.com>
2019-10-15 13:49:23 +0300

    Added : BaseViewBindingPagerAdapter with ViewPager

---
+++ basecorelibrary/src/main/java/com/yenen/ahmet/basecorelibrary/base/adapter/BaseViewBindingPagerAdapter.kt
@@ -0,0 +1,55 @@
+package com.yenen.ahmet.basecorelibrary.base.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.viewpager.widget.PagerAdapter
+
+abstract class BaseViewBindingPagerAdapter<VDB : ViewDataBinding, T>(
+    @LayoutRes private val layoutRes: Int, private val items: MutableList<T>,
+    private var listener: Listener<VDB, T>?
+) : PagerAdapter() {
+
+
+    interface Listener<VDB : ViewDataBinding, T> {
+        fun setBindingModel(binding: VDB, item: T)
+    }
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val inflater = LayoutInflater.from(container.context)
+        val binding =
+            DataBindingUtil.inflate<ViewDataBinding>(inflater, layoutRes, container, false) as VDB
+        container.addView(binding.root)
+        listener?.setBindingModel(binding, items[position])
+        binding.executePendingBindings()
+        return binding.root
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        container.removeView(`object` as View)
+    }
+
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view === `object`
+    }
+
+    override fun getCount(): Int = items.size
+
+    fun getItem(position:Int) :T{
+        return items[position]
+    }
+
+    fun clear(){
+        items.clear()
+    }
+
+
+
+    fun unBind() {
+        listener = null
+    }
+}
