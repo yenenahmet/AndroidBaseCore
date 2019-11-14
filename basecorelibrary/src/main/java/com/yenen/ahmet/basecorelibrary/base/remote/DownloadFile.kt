880f75a89bf369d708577702c3d78352371ceea2
Ahmet YENEN <ahmet.yenen@spexco.com>
2019-11-14 14:11:44 +0300

    Added : DownloadFile writeToDisk

---
+++ basecorelibrary/src/main/java/com/yenen/ahmet/basecorelibrary/base/remote/DownloadFile.kt
@@ -0,0 +1,50 @@
+package com.yenen.ahmet.basecorelibrary.base.remote
+
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.lang.Exception
+
+class DownloadFile constructor(
+    private val inputStream: InputStream,
+    private val path: String,
+    private val subPath: String,
+    private val listener: DownloadFileListener
+) {
+
+    fun writeInputStreamToDisk() {
+        try {
+            val filePath = File(path)
+            val file = File("${filePath.path}/$subPath")
+
+            if (!filePath.exists())
+                filePath.mkdirs()
+
+            var fileSizeDownloaded = 0L
+
+
+            val fileReader = ByteArray(4096)
+            FileOutputStream(file).use {
+                inputStream.use { inputStream ->
+
+                    while (true) {
+
+                        val read = inputStream.read(fileReader)
+
+                        if (read == -1)
+                            break
+
+                        it.write(fileReader, 0, read)
+
+                        fileSizeDownloaded += read
+                    }
+                }
+                it.flush()
+                listener.onSuccess(fileSizeDownloaded)
+            }
+        } catch (ex: Exception) {
+            listener.onError(ex)
+        }
+
+    }
+}
---
+++ basecorelibrary/src/main/java/com/yenen/ahmet/basecorelibrary/base/remote/DownloadFileListener.kt
@@ -0,0 +1,8 @@
+package com.yenen.ahmet.basecorelibrary.base.remote
+
+import java.lang.Exception
+
+interface DownloadFileListener {
+    fun onError(ex:Exception)
+    fun onSuccess(fileSizeDownloaded:Long)
+}
