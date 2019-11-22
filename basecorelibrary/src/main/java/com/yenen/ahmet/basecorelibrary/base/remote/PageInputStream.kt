30be311735b4ed9cc7f2fbabebb3e084c463d993
Ahmet YENEN <ahmet.yenen@spexco.com>
2019-11-22 15:12:12 +0300

    Added : Rx Flowable InputStream

---
+++ basecorelibrary/src/main/java/com/yenen/ahmet/basecorelibrary/base/remote/PageInputStream.kt
@@ -0,0 +1,39 @@
+package com.yenen.ahmet.basecorelibrary.base.remote
+
+import io.reactivex.Emitter
+import io.reactivex.Flowable
+import io.reactivex.functions.BiConsumer
+import io.reactivex.functions.Consumer
+import java.io.BufferedInputStream
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.URL
+import java.util.concurrent.Callable
+
+class PageInputStream {
+
+    fun getFlowableData(url: String,charsetName:String): Flowable<String> {
+        return Flowable.generate(
+            Callable {
+                val urlConnection = URL(url)
+                val bufferedInputStream = BufferedInputStream(urlConnection.openStream())
+                BufferedReader(InputStreamReader(bufferedInputStream, charsetName))
+            },
+            BiConsumer<BufferedReader, Emitter<String>> { bufferedReader, stringEmitter ->
+                val line = bufferedReader.readLine()
+                if (line != null) {
+                    stringEmitter.onNext(line)
+                } else {
+                    stringEmitter.onComplete()
+                }
+            }, Consumer { bufferedReader ->
+                try {
+                    bufferedReader.close()
+                } catch (ignore: Exception) {
+
+                }
+            }
+
+        )
+    }
+}
