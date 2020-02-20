86ba84add14964fb1882693653d48d1e5585dd21
Ahmet YENEN <ahmet.yenen@spexco.com>
2020-02-20 14:03:13 +0300

    Added : DateUtils

---
+++ basecorelibrary/src/main/java/com/yenen/ahmet/basecorelibrary/base/utility/DateUtils.kt
@@ -0,0 +1,29 @@
+package com.yenen.ahmet.basecorelibrary.base.utility
+
+import android.annotation.SuppressLint
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
+
+object DateUtils {
+    val monthNames =
+        arrayOf("Oca", "Şub", "Mar", "Nis", "May", "Haz", "Tem", "Ağu", "Eyl", "Eki", "Kas", "Ara")
+
+    val dfServer = "yyyy-MM-dd'T'HH:mm:ss.SSS"
+
+    @SuppressLint("SimpleDateFormat")
+    fun getServerDateTimeFormatShort(dateTime: String): String {
+        try {
+            val simpleDateFormat = SimpleDateFormat(dfServer)
+            val date = simpleDateFormat.parse(dateTime)
+            val cal = Calendar.getInstance()
+            cal.time = date!!
+            val day = cal.get(Calendar.DAY_OF_MONTH).toString()
+            val mont = monthNames[cal.get(Calendar.MONTH)]
+            val time = "${cal.get(Calendar.HOUR_OF_DAY)}:${cal.get(Calendar.MINUTE)}"
+            return "$day $mont $time"
+        } catch (ex: Exception) {
+            return ""
+        }
+    }
+}
