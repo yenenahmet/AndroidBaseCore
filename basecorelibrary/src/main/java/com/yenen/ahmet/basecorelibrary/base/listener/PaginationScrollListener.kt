9d7abcf34fcaf63fb6e7c24ccee1c3a752a046df
yenenahmet <ynnahmet@gmail.com>
2020-05-23 17:52:17 +0300

    Added:  BaseApplication, AdapterBin

--- basecorelibrary/src/main/java/com/yenen/ahmet/basecorelibrary/base/listener/PaginationScrollListener.kt
+++ basecorelibrary/src/main/java/com/yenen/ahmet/basecorelibrary/base/listener/PaginationScrollListener.kt
@@ -10,14 +10,15 @@ abstract class PaginationScrollListener constructor(private val layoutManager: L
     override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
         super.onScrolled(recyclerView, dx, dy)
 
-        val visibleItemCount = layoutManager.getChildCount()
-        val totalItemCount = layoutManager.getItemCount()
+        val visibleItemCount = layoutManager.childCount
+        val totalItemCount = layoutManager.itemCount
         val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
         val isTotalItemCount = (visibleItemCount + firstVisibleItemPosition) >= totalItemCount
 
         if (!isLoading() && !isLastPage() && isTotalItemCount && firstVisibleItemPosition >= 0) {
             loadMoreItems()
         }
+
     }
 
     protected abstract fun loadMoreItems()
