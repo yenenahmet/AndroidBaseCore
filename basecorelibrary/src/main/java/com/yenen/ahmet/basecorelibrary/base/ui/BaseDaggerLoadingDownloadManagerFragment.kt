f9db95979a98f4857078e650ccac2e103c36d06a
Ahmet YENEN <ahmet.yenen@spexco.com>
2020-06-19 16:11:49 +0300

    review: BaseDaggerLoadingDownloadManagerFragment

--- basecorelibrary/src/main/java/com/yenen/ahmet/basecorelibrary/base/ui/BaseDaggerLoadingDownloadManagerFragment.kt
+++ basecorelibrary/src/main/java/com/yenen/ahmet/basecorelibrary/base/ui/BaseDaggerLoadingDownloadManagerFragment.kt
@@ -4,11 +4,13 @@ import android.app.DownloadManager
 import android.content.Context
 import android.content.IntentFilter
 import android.net.Uri
+import android.os.Environment
 import androidx.annotation.LayoutRes
 import androidx.databinding.ViewDataBinding
 import com.yenen.ahmet.basecorelibrary.base.download.CompleteListener
 import com.yenen.ahmet.basecorelibrary.base.download.DownloadManagerListener
 import com.yenen.ahmet.basecorelibrary.base.viewmodel.BaseViewModel
+import java.lang.Exception
 
 abstract class BaseDaggerLoadingDownloadManagerFragment<VM : BaseViewModel, DB : ViewDataBinding, VDB : ViewDataBinding>(
     viewModelClass: Class<VM>, @LayoutRes private val layoutRes: Int, @LayoutRes private val loadingLayoutResId: Int
@@ -19,7 +21,7 @@ abstract class BaseDaggerLoadingDownloadManagerFragment<VM : BaseViewModel, DB :
 
     override fun onResult(status: Int, reason: Int, requestId: Long,uri:Uri?,mimeType:String) {
         removeDownloadRequestId(requestId)
-        onCompleted(status, reason, requestId,uri,mimeType)
+        onCompleted(status, reason, requestId,uri,mimeType,"")
     }
 
     override fun onResume() {
@@ -32,15 +34,15 @@ abstract class BaseDaggerLoadingDownloadManagerFragment<VM : BaseViewModel, DB :
         activity?.registerReceiver(downloadManagerListener, filter)
     }
 
-    protected fun addDownloadRequestId(requestId: Long){
+    private fun addDownloadRequestId(requestId: Long) {
         downloadManagerListener?.addRequestIds(requestId)
     }
 
-    protected fun removeDownloadRequestId(requestId: Long){
+    protected fun removeDownloadRequestId(requestId: Long) {
         downloadManagerListener?.removeRequestIds(requestId)
     }
 
-    protected fun clearDownloadRequestIds(){
+    protected fun clearDownloadRequestIds() {
         downloadManagerListener?.clearRequestIds()
     }
 
@@ -53,6 +55,42 @@ abstract class BaseDaggerLoadingDownloadManagerFragment<VM : BaseViewModel, DB :
         downloadManagerListener=null
     }
 
-    protected abstract fun onCompleted(status: Int, reason: Int, requestId: Long,uri:Uri?,mimeType: String)
+    protected fun downloadManagerDownload(
+        notificationVisibility: Int,
+        path: String,
+        fileName: String,
+        token: String?
+    ) {
+        try {
+            val request = DownloadManager.Request(Uri.parse(path)).apply {
+                setTitle(fileName)
+                setDescription("Dosya indiriliyor...")
+                setNotificationVisibility(notificationVisibility)
+                setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName)
+                setAllowedOverMetered(true)
+                setAllowedOverRoaming(true)
+                setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE or DownloadManager.Request.NETWORK_WIFI)
+                token?.let {
+                    addRequestHeader("Authorization", it)
+                }
+            }
+            val downloadManager = activity?.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+            val req = downloadManager.enqueue(request)
+            addDownloadRequestId(req)
+
+        } catch (ex: Exception) {
+            onCompleted(-1, -1, -1, null, "", ex.toString())
+        }
+
+    }
+
+    protected abstract fun onCompleted(
+        status: Int,
+        reason: Int,
+        requestId: Long,
+        uri: Uri?,
+        mimeType: String,
+        err: String
+    )
 
 }
