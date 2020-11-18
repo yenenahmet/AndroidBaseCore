031cbb4083a29852a3df8cde334b2f54b23e2ef0
Ahmet YENEN <ahmet.yenen@spexco.com>
2020-11-18 14:17:12 +0300

    review

--- basecorelibrary/src/main/java/com/yenen/ahmet/basecorelibrary/base/gallery/ImageGalleryAdapter.kt
+++ basecorelibrary/src/main/java/com/yenen/ahmet/basecorelibrary/base/gallery/ImageGalleryAdapter.kt
@@ -1,9 +1,12 @@
 package com.yenen.ahmet.basecorelibrary.base.gallery
 
+import android.app.Activity
 import com.bumptech.glide.Glide
 import com.yenen.ahmet.basecorelibrary.R
 import com.yenen.ahmet.basecorelibrary.base.adapter.BaseViewBindingPagerAdapter
 import com.yenen.ahmet.basecorelibrary.databinding.ImageGalleryPagerItemBinding
+import com.ablanco.zoomy.Zoomy
+
 
 class ImageGalleryAdapter : BaseViewBindingPagerAdapter<ImageGalleryPagerItemBinding, String>(
     R.layout.image_gallery_pager_item,
@@ -15,6 +18,9 @@ class ImageGalleryAdapter : BaseViewBindingPagerAdapter<ImageGalleryPagerItemBin
         position: Int
     ) {
         Glide.with(binding.root.context).load(item).into(binding.img)
+
+        val builder = Zoomy.Builder(binding.root.context as Activity).target(binding.img)
+        builder.register()
     }
 
 
