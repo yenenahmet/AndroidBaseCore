6e425deadadfac5cf380c90bed3bcfe1a74f3f8d
Ahmet YENEN <ahmet.yenen@spexco.com>
2020-11-18 15:59:45 +0300

    review

--- basecorelibrary/src/main/java/com/yenen/ahmet/basecorelibrary/base/gallery/ImageGalleryActivity.kt
+++ basecorelibrary/src/main/java/com/yenen/ahmet/basecorelibrary/base/gallery/ImageGalleryActivity.kt
@@ -2,8 +2,12 @@ package com.yenen.ahmet.basecorelibrary.base.gallery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.viewpager.widget.ViewPager
 import com.yenen.ahmet.basecorelibrary.R
+import com.yenen.ahmet.basecorelibrary.base.extension.screenBarClear
+import ru.tinkoff.scrollingpagerindicator.ScrollingPagerIndicator
+import java.lang.Exception
 
 class ImageGalleryActivity : AppCompatActivity() {
 
@@ -17,10 +21,26 @@ class ImageGalleryActivity : AppCompatActivity() {
         intent.getStringArrayExtra("IMAGES")?.let {
             imgAdapter.setItems(it.toList())
         }
+        val position = intent.getIntExtra("POSITION",0)
 
         pager.adapter = imgAdapter
         pager.setPageTransformer(true, ZoomOutPageTransformer())
 
+        try{
+            pager.setCurrentItem(position,true)
+        }catch (ex:Exception){
+
+        }
+
+        val back = findViewById<AppCompatImageView>(R.id.imgClose)
+        back.setOnClickListener {
+            finish()
+        }
+
+        val indicator = findViewById<ScrollingPagerIndicator>(R.id.indicator)
+        indicator.attachToPager(pager)
+
+        screenBarClear()
     }
 
 
