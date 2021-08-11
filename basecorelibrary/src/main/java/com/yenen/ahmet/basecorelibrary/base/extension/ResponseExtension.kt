3f02b874a8b580224258bb1f8e4d3629e3ee76d1
yenenahmet <yenenahmet@hotmail.com>
2021-08-11 13:55:24 +0300

    [master] *Add handleOnlySuccessStatusWithLoading for Activity

--- basecorelibrary/src/main/java/com/yenen/ahmet/basecorelibrary/base/extension/ResponseExtension.kt
+++ basecorelibrary/src/main/java/com/yenen/ahmet/basecorelibrary/base/extension/ResponseExtension.kt
@@ -1,6 +1,7 @@
 package com.yenen.ahmet.basecorelibrary.base.extension
 
 import com.yenen.ahmet.basecorelibrary.base.model.LiveDataResponseModel
+import com.yenen.ahmet.basecorelibrary.base.ui.BaseLoadingActivity
 import com.yenen.ahmet.basecorelibrary.base.ui.BaseLoadingFragment
 import com.yenen.ahmet.basecorelibrary.base.utility.ProjectConstants
 
@@ -17,6 +18,23 @@ fun <T> LiveDataResponseModel<T>.handleOnlySuccessStatusWithLoading(
                 block.invoke(it)
             }
             fragment.dismissDialog()
+        }
+    }
+}
+
+fun <T> LiveDataResponseModel<T>.handleOnlySuccessStatusWithLoading(
+    block: (T) -> Unit, activity: BaseLoadingActivity<*, *, *>
+) {
+    when (status) {
+        ProjectConstants.ERROR_STATUS -> {
+            activity.showToast(description ?: "")
+            activity.dismissDialog()
+        }
+        ProjectConstants.SUCCESS_STATUS -> {
+            this.data?.let {
+                block.invoke(it)
+            }
+            activity.dismissDialog()
         }
     }
 }
